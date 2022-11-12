47366c6fd77b359b0602711003418ec9a630acdf
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2022-11-12 22:11:47 +0530

    Done

--- Go/Assignment/My_blkc.go
+++ Go/Assignment/My_blkc.go
@@ -4,58 +4,87 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"math"
+	"math/big"
+	"time"
 )
 
-type BlockChain struct {
-	blocks []*Block
-}
+const (
+	difficulty = 15
+)
 
 type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
+	Index     int
+	Timestamp int64
+	Hash      []byte
+	Data      string
+	PrevHash  []byte
 }
 
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
+// bytes manipulation based on https://github.com/Jeiwan/blockchain_go
+
+func genhash(data string, prev []byte) []byte {
+	head := bytes.Join([][]byte{prev, []byte(data)}, []byte{})
+
+	h32 := sha256.Sum256(head)
+
+	fmt.Printf("Header hash: %x\n", h32)
+
+	return h32[:]
 }
 
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
+func mine(hash []byte) []byte {
+	target := big.NewInt(1)
+	target = target.Lsh(target, uint(256-difficulty))
+
+	fmt.Printf("target: %x\n", target)
+
+	var nonce int64
+
+	for nonce = 0; nonce < math.MaxInt64; nonce++ {
+		testNum := big.NewInt(0)
+		testNum.Add(testNum.SetBytes(hash), big.NewInt(nonce))
+		testHash := sha256.Sum256(testNum.Bytes())
+
+		fmt.Printf("\rproof: %x (nonce: %d)", testHash, nonce)
+
+		if target.Cmp(testNum.SetBytes(testHash[:])) > 0 {
+			fmt.Println("\nFound!")
+			return testHash[:]
+		}
+	}
+
+	return []byte{}
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+func NewBlock(id int, data string, prev []byte) *Block {
+	return &Block{
+		id,
+		time.Now().Unix(),
+		mine(genhash(data, prev)),
+		data,
+		prev,
+	}
 }
 
 func main() {
-	chain := InitBlockChain()
+	bdatas := []string{"\n1\n    Name:Priyanshu Batham\n    Email:priyanshubatham24733@gmail.com\n    Phno:7905570261\n    Blood group:B+\n",
+		"\n2\n    Name:Pihu Gautam\n    Email:piihhuu123@gmail.com\n    Phno:8473815186\n    Blood group:A-\n",
+		"\n3\n    Name:Abhishek Yadav\n    Email:abhistark45@gmail.com\n    Phno:9456187265\n    Blood group:O+\n",
+		"\n4\n    Name:Srijan Upadhyay\n    Email:legend_extreme106@gmail.com\n    Phno:9461852345\n    Blood group:B-\n",
+		"\n5\n    Name:Gaurav Singh\n    Email:Gaurav_here69@gmail.com\n    Phno:9451286452\n    Blood group:A+\n",
+		"\n6\n    Name:Garima Agnihotri\n    Email:GarimaAgnihotri11@gmail.com\n    Phno:9451287645\n    Blood group:O-\n"}
 
-	chain.AddBlock("1\n    Name:Priyanshu Batham\n    Email:priyanshubatham24733@gmail.com\n    Phno:7905570261\n    Blood group:B+")
-	chain.AddBlock("2\n    Name:Pihu Gautam\n    Email:piihhuu123@gmail.com\n    Phno:8473815186\n    Blood group:A-")
-	chain.AddBlock("3\n    Name:Abhishek Yadav\n    Email:abhistark45@gmail.com\n    Phno:9456187265\n    Blood group:O+")
-	chain.AddBlock("4\n    Name:Srijan Upadhyay\n    Email:legend_extreme106@gmail.com\n    Phno:9461852345\n    Blood group:B-")
-	chain.AddBlock("5\n    Name:Gaurav Singh\n    Email:Gaurav_here69@gmail.com\n    Phno:9451286452\n    Blood group:A+")
-	chain.AddBlock("6\n    Name:Garima Agnihotri\n    Email:GarimaAgnihotri11@gmail.com\n    Phno:9451287645\n    Blood group:O-")
+	prev := []byte{}
 
-	for _, block := range chain.blocks {
-		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data in Block: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println("\n")
+	for i, d := range bdatas {
+		b := NewBlock(i, d, prev)
+		fmt.Printf("Id: %d\nHash; %x\nData: %s\nPrevious: %x\n",
+			b.Index,
+			b.Hash,
+			b.Data,
+			b.PrevHash,
+		)
+		prev = b.Hash
 	}
 }
