00e4501064a36640d45674374c117e807d526ea9
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2022-12-02 22:44:46 +0530

    mehnat:100 output:0

--- C/Advanced/AQ2.C
+++ C/Advanced/AQ2.C
@@ -1,37 +1,18 @@
-//replacing spaces with special character
 #include<stdio.h>
 #include<conio.h>
 int main(){
-  char str[50],newstr[70];
-  char spc;
-  int i,j,ctr=0;
-  //clrscr();
-  fflush(stdin);
-  printf("Enter a sentence(upto 50 chars):\n");
-  scanf(" %[^\n]",&str);
-  printf("\nEnter the special symbol:");
-  scanf(" %c",&spc);
-  for(j=0;j<50;j++){
-    if(str[j]=='\0'){
-      break;
-    }
-    ctr++;
-  }
-  for(i=0;i<ctr;i++){
-
-    //replacing spaces
+  int i;
+  char str[50],sym;
+  clrscr();
+  printf("Enter a string:");
+  gets(str);
+  printf("Enter a symbol:");
+  scanf("%c",&sym);
+  for(i=0;i<50;i++){
     if(str[i]==' '){
-      newstr[i]=spc;
-    }
-
-    else{
-      newstr[i]=str[i];
+      str[i]=sym;
     }
   }
-  newstr[ctr]='\0'; //putting the finisher at the end of newstring
-  printf("New string:\n");
-  printf("%s",newstr);
-
+  puts(str);
   getch();
-  return 0;
 }
--- C/Advanced/AQ2VR2.C
+++ C/Advanced/AQ2VR2.C
@@ -1,18 +1,37 @@
+//replacing spaces with special character
 #include<stdio.h>
 #include<conio.h>
 int main(){
-  int i;
-  char str[50],sym;
-  clrscr();
-  printf("Enter a string:");
-  gets(str);
-  printf("Enter a symbol:");
-  scanf("%c",&sym);
-  for(i=0;i<50;i++){
+  char str[50],newstr[70];
+  char spc;
+  int i,j,ctr=0;
+  //clrscr();
+  fflush(stdin);
+  printf("Enter a sentence(upto 50 chars):\n");
+  scanf(" %[^\n]",&str);
+  printf("\nEnter the special symbol:");
+  scanf(" %c",&spc);
+  for(j=0;j<50;j++){
+    if(str[j]=='\0'){
+      break;
+    }
+    ctr++;
+  }
+  for(i=0;i<ctr;i++){
+
+    //replacing spaces
     if(str[i]==' '){
-      str[i]=sym;
+      newstr[i]=spc;
+    }
+
+    else{
+      newstr[i]=str[i];
     }
   }
-  puts(str);
+  newstr[ctr]='\0'; //putting the finisher at the end of newstring
+  printf("New string:\n");
+  printf("%s",newstr);
+
   getch();
+  return 0;
 }
